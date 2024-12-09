4d76579f9238996860b6b3e7fbf8fc22e798e6f8
MyangSZ <unoyou@naver.com>
2024-12-09 16:22:05 +0900

    oz 미니프로젝트
    영화소개 사이트 만들기

--- src/components/MovieDetail.jsx
+++ src/components/MovieDetail.jsx
@@ -1,25 +1,31 @@
 import { useParams } from "react-router";
-
+import movieinfo from "../data/movieDetailData.json";
 import movielist from "../data/movieListData.json";
 import styled from "styled-components";
+import { useState } from "react";
 
 function MovieDetail() {
   const { results } = movielist;
   const { movie_id } = useParams();
+  const [movie] = useState(movieinfo);
 
-  const moviedetail = results.find((id) => id.id === Number(movie_id));
+  // const moviedetail = results.find((id) => id.id === Number(movie_id));
   // const { title, average, genres, overview } = moviedetail;
   return (
     <>
       <MovieInfo>
         <img
           className="img"
-          src={`https://image.tmdb.org/t/p/w500${moviedetail.poster_path}`}
+          src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
         />
-        <p className="title">{moviedetail.title}</p>
-        <p className="average">{moviedetail.vote_average}</p>
-        <p className="genres">{moviedetail.genres}</p>
-        <p className="overview">{moviedetail.overview}</p>
+        <p className="title">{movie.title}</p>
+        <p className="average">{movie.vote_average}</p>
+        <div className="genres">
+          {movie.genres.map((genre) => (
+            <span key={genre.id}>{genre.name}</span>
+          ))}
+        </div>
+        <p className="overview">{movie.overview}</p>
       </MovieInfo>
     </>
   );
@@ -60,6 +66,8 @@ const MovieInfo = styled.div`
   }
   .genres {
     grid-area: genres;
+    font-size: 30px;
+    padding-left: 10px;
   }
   .overview {
     grid-area: overview;
